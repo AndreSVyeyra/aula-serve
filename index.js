f7f1dbd75dbc6f8355247e649795c18f14c7b9d5
Diogo Costa de Paula <costadiogo30@gmail.com>
2020-09-12 21:33:55 -0300

    Criado a rota do método get

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
- const express = require ('express');
+const express = require ('express');
 
- console.log(express);
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('hello world');
+});
+
+
+
+
+app.listen(3000, () => {
+    console.log('Servidor rodando na porta 3000')
+});
